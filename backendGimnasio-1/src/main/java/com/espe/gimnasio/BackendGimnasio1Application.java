e19e4fbf72cb97320b57a0d45680417154ee485f
Raúl Andrés Silva Velásquez <raul29247@gmail.com>
2025-05-14 00:05:49 -0500

    login y registro

---
+++ backendGimnasio-1/src/main/java/com/espe/gimnasio/BackendGimnasio1Application.java
@@ -0,0 +1,13 @@
+package com.espe.gimnasio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGimnasio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGimnasio1Application.class, args);
+	}
+
+}
